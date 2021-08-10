29a1d212e1ab92d0a606fd5ea978d3dba9bc9f9e
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-10 23:09:00 +0530

    Welcome To Snake Ladder Game

---
+++ src/com/ram/snakeladder.java
@@ -0,0 +1,10 @@
+package com.ram;
+
+import java.sql.SQLOutput;
+
+public class snakeladder {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake Ladder Program");
+    }
+}
